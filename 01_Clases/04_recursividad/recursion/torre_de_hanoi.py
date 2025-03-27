47044d4cc0f70d18fc33ddeea36598a8c2050711
CaidevOficial <ffalcone@fra.utn.edu.ar>
2025-03-27 19:07:53 -0300

    🚀 Update torre de hanoi

--- 01_Clases/04_recursividad/recursion/torre_de_hanoi.py
+++ 01_Clases/04_recursividad/recursion/torre_de_hanoi.py
@@ -1,6 +1,8 @@
+movimientos = {
+    "movimientos": 0
+}
 
-
-def torre_de_hanoi(cant_discos: int, nombre_torre_origen: str, 
+def torre_de_hanoi(mov: dict, cant_discos: int, nombre_torre_origen: str, 
                    nombre_torre_destino: str, nombre_torre_aux: str) -> None:
     
     texto =\
@@ -16,10 +18,13 @@ f"""
     
     if cant_discos == 1:
         print(f'Se movio el disco 1 desde {nombre_torre_origen} a {nombre_torre_destino}')
+        mov['movimientos'] += 1
     else:
-        torre_de_hanoi(cant_discos - 1, nombre_torre_origen, nombre_torre_aux, nombre_torre_destino)
+        torre_de_hanoi(mov, cant_discos - 1, nombre_torre_origen, nombre_torre_aux, nombre_torre_destino)
+        mov['movimientos'] += 1
         print(f'Se movio el disco {cant_discos} desde {nombre_torre_origen} a {nombre_torre_destino}')
-        torre_de_hanoi(cant_discos - 1, nombre_torre_aux, nombre_torre_destino, nombre_torre_origen)
+        torre_de_hanoi(mov, cant_discos - 1, nombre_torre_aux, nombre_torre_destino, nombre_torre_origen)
+        mov['movimientos'] += 1
 
-
-torre_de_hanoi(3, "Torre 1", "Torre 2", "Torreón 3")
+torre_de_hanoi(movimientos, 7, "Torre 1", "Torre 2", "Torreón 3")
+print(f'Movimientos: {movimientos["movimientos"]}')
